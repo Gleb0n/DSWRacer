eb9571180f5c912ae105138793f8b6c0adacb9c5
Gleb0n <dsw49739@student.dsw.edu.pl>
2024-05-28 13:48:50 +0200

    Update #7

--- Assets/Scripts/AI/CarAI.cs
+++ Assets/Scripts/AI/CarAI.cs
@@ -10,11 +10,15 @@ public class CarAI : Car
     private int currentCheckpointCount;
 
     private GameObject tracker;
+    private float currentTrackerSpeed;
+
+    private CarController player;
 
     private void Awake()
     {
         tracker = GameObject.CreatePrimitive(PrimitiveType.Cube);
         Destroy(tracker.GetComponent<Collider>());
+        Destroy(tracker.GetComponent<MeshRenderer>());
         tracker.transform.position = this.transform.position;
         tracker.transform.rotation = this.transform.rotation;   
 
@@ -23,25 +27,20 @@ public class CarAI : Car
     {
         SetUpWheels();
         carRigidbody = GetComponent<Rigidbody>();
+        player = FindObjectOfType<CarController>();
         checkpointSingles = trackCheckpoints.GetCheckpoints();
+        currentTrackerSpeed = trackerSpeed;
     }
-
 
     private void FixedUpdate()
     {  
         ProgressTracker();
-
-        Vector3 normalizedDirection = (tracker.transform.position - this.transform.position).normalized;
-        Quaternion lookAtCheckpoint = Quaternion.LookRotation(tracker.transform.position - this.transform.position);
-
-        ApplyTorque(normalizedDirection.z, _motorTorque);
-        UpdateRotation(lookAtCheckpoint.normalized.y, _steerAngle);
-        Debug.Log(lookAtCheckpoint.normalized.y);
+        FollowTracker();
     }
     private void ProgressTracker()
     {
         Vector3 currentCheckpointPosition = checkpointSingles[currentCheckpointCount].gameObject.transform.position;
-        if(Vector3.Distance(tracker.transform.position, currentCheckpointPosition) < 3 )
+        if(Vector3.Distance(tracker.transform.position, currentCheckpointPosition) < 5 )
         {
             currentCheckpointCount++;
         }
@@ -51,6 +50,58 @@ public class CarAI : Car
         }
 
         tracker.transform.LookAt(currentCheckpointPosition);
-        tracker.transform.Translate(0, 0, carRigidbody.velocity.magnitude * trackerSpeed * Time.deltaTime);
+        tracker.transform.Translate(0, 0, (carRigidbody.velocity.magnitude * currentTrackerSpeed) * Time.deltaTime);
+    }
+    private void FollowTracker()
+    {
+
+        Vector3 normalizedDirection = (tracker.transform.position - this.transform.position).normalized;
+        float lookAtTracker = Vector3.SignedAngle(transform.forward, normalizedDirection, Vector3.up);
+
+        lookAtTracker = Mathf.Clamp(lookAtTracker / 100f, -1, 1);
+        if (IsWaitingForCar(20))
+        {
+            currentTrackerSpeed = 0;
+        }
+        else
+        {
+            currentTrackerSpeed = trackerSpeed;
+        }
+
+        if (IsWaitingForPlayer(100f))
+        {
+            ApplyTorque(Mathf.Abs(normalizedDirection.z), _motorTorque / 5);
+            UpdateRotation(lookAtTracker, _steerAngle);
+        }
+        else
+        {
+            ApplyTorque(Mathf.Abs(normalizedDirection.z), _motorTorque);
+            UpdateRotation(lookAtTracker, _steerAngle);
+        }
+        
+    }
+
+    private bool IsWaitingForCar(float distanceToCar)
+    {
+        if (Vector3.Distance(tracker.transform.position, transform.position) > distanceToCar)
+        {
+            return true;
+        }
+        return false;
+    }
+    private bool IsWaitingForPlayer(float distanceToPlayer)
+    {
+        if (player == null)
+        {
+            return false;
+        }
+        else
+        {
+            if (Vector3.Distance(transform.position, player.transform.position) > distanceToPlayer)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
